b2cf0e8a7d1bad2081a90ebfc3f01f1b402ff2d4
Sébastien Deleuze <seb@deleuze.fr>
2012-12-06 23:31:12 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -158,6 +158,6 @@ html_sidebars = {
                  'sourcelink.html', 'searchbox.html']
 }
 
-html_title = "RESThub, your Spring + Backbone.js stack"
+html_title = "Your Spring + Backbone.js stack"
 html_short_title = "RESThub 2.0"
 
